5a4b2a437a55e539fbfc3e9b25d6a63e78a6f6ef
HanamiGeto <tidu11@hotmail.de>
2023-03-11 13:04:52 +0100

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,12 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { GameInfoComponent } from './game-info/game-info.component';
 import { MatCardModule } from '@angular/material/card';
+// import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 
 @NgModule({
@@ -36,7 +42,12 @@ import { MatCardModule } from '@angular/material/card';
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
-    MatCardModule
+    MatCardModule,
+    // provideFirebaseApp(() => initializeApp(environment.firebase)),
+    AngularFireModule.initializeApp(environment.firebase),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDHvRDzYcXg_XsxKnzJeO8oYIO41CBx-fc",
+    authDomain: "ring-of-fire-cde3d.firebaseapp.com",
+    projectId: "ring-of-fire-cde3d",
+    storageBucket: "ring-of-fire-cde3d.appspot.com",
+    messagingSenderId: "768746327977",
+    appId: "1:768746327977:web:128b44e420e36b88163eb8"
+  }
+}
